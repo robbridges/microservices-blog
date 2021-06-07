22ddfaba0efb3358228cf0d66a546a30e0c37bd9
robbridges <robbridges@users.noreply.github.com>
2021-06-07 13:31:40 -0700

    wiring up react

---
+++ client/src/postCreate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const postCreate = () => {
+  return ( <div>
+    <form>
+      <div className="form-group">
+        <label>Title</label>
+        <input className="form-control" />
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  </div>
+  );
+}
+
+export default postCreate;
