c11cbed4255ecaccfdf54dd397b725d41c244322
robbridges <robbridges@users.noreply.github.com>
2021-06-08 21:08:07 -0700

    Still wiring up query listener

--- posts/index.js
+++ posts/index.js
@@ -34,6 +34,12 @@ app.post('/posts', async (req, res) => {
 
 });
 
+app.post('/events', (req, res) => {
+  console.log('Recieved Event', req.body.type);
+
+  res.send({});
+});
+
 app.listen(4000, () => {
   console.log('Listening on port 4000');
 });
